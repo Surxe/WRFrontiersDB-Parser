f0ec9130f1cebbb3585045dcf24df1c174144567
Surxe <119145352+Surxe@users.noreply.github.com>
2025-07-21 23:07:23 -0500

    progression table init concept
    finish _p_level_reward

---
+++ src/parsers/progression_table.py
@@ -0,0 +1,60 @@
+# Add parent dirs to sys path
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from parsers.object import Object
+
+
+
+class ProgressionTable(Object):
+    objects = dict()  # Dictionary to hold all ProgressionTable instances
+
+    def _parse(self):
+        props = self.source_data["Properties"]
+
+        key_to_parser_function = {
+            "Levels": self._p_levels,
+            "ID": None,
+        }
+
+        self._process_key_to_parser_function(key_to_parser_function, props, tabs=2)
+
+    def _p_levels(self, levels: list):
+        parsed_levels = dict()
+
+        for i, level in enumerate(levels):
+            level_number = i+1
+            key_to_parser_function = {
+                "ReputationCost": "value",
+                "Reward": (self._p_level_reward, "rewards")
+            }
+
+            parsed_level = self._process_key_to_parser_function(key_to_parser_function, level, set_attrs=False, log_descriptor='_p_levels()', tabs=3)
+            parsed_levels[level_number] = parsed_level
+
+        return parsed_levels
+
+    def _p_level_reward(self, reward: dict):
+        key_to_parser_function = {
+            "Currencies": "value",
+            #TODO
+        }
+
+        parsed_reward = self._process_key_to_parser_function(key_to_parser_function, reward, set_attrs=False, log_descriptor='_p_level_reward()', tabs=4)
+        
+        # Remove empty lists and dicts
+        parsed_reward = {k: v for k, v in parsed_reward.items() if v not in ([], {}, None)}
+
+        return parsed_reward
+
+def parse_progression_table():
+    progression_table_path = r"WRFrontiers/Content/Sparrow/Mechanics/DA_ProgressionTable.json"
+
+    # Hero pilots are in this dir directly
+    progression_table = ProgressionTable.get_from_asset_path(progression_table_path)
+
+    ProgressionTable.to_file()
+
+if __name__ == "__main__":
+    parse_progression_table()
