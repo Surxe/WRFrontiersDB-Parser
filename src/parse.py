ae9524c2cabf26bfbbb41c5407e8c3b22eb4e872
Surxe <119145352+Surxe@users.noreply.github.com>
2025-07-21 22:22:41 -0500

    todo

--- src/parse.py
+++ src/parse.py
@@ -13,5 +13,5 @@ clear_dir("output")  # Clear the data directory before parsing
 
 parse_localizations()
 print()
-#parse_modules() #module relies on english localization being added to each key just as a helpful Ctrl+F reference
+parse_modules() #module relies on english localization being added to each key just as a helpful Ctrl+F reference
 parse_pilots()  # Pilot parser relies on module data being parsed first
