b0dc9e407ce2fa5a93a2a773fd4695b143d79c92
Surxe <119145352+Surxe@users.noreply.github.com>
2025-08-15 11:45:25 -0500

    log production_status

--- src/parsers/module.py
+++ src/parsers/module.py
@@ -58,7 +58,10 @@ class Module(Object):
             }
             
             self._process_key_to_parser_function(key_to_parser_function, props, tabs=2)
-    
+
+        if not hasattr(self, "production_status"):
+            log(f"Module {self.id} is not ready for production", tabs=1)
+
     def _p_module_rarity(self, data):
         asset_path = data["ObjectPath"]
         return ModuleRarity.get_from_asset_path(asset_path)
