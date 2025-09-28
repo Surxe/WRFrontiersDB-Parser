c4ea8cc51bbb4188ba165cb3e1c158cd3310a0d3
Surxe <119145352+Surxe@users.noreply.github.com>
2025-09-28 13:49:42 -0500

    import run_process from deadbot repo

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import os
 import json
 import re
 import sys
+import subprocess
+import os
 from loguru import logger
 load_dotenv()
 
@@ -287,7 +289,7 @@ def to_snake_case(text):
 
 
 ###############################
-#        Dictionary          #
+#        Dictionary           #
 ###############################
 
 def sort_dict(d: dict, num_levels: int = -1) -> dict:
@@ -520,3 +522,37 @@ def process_key_to_parser_function(key_to_parser_function_map, data, obj=None, l
 
     if not set_attrs:
         return parsed_data
+    
+
+###############################
+#           Process           #
+###############################
+
+def run_process(params, name=''):
+    """Runs a subprocess with the given parameters and logs its output line by line
+
+    Args:
+        params (list[str] | str): The command and arguments to execute
+        name (str, optional): An optional name to identify the process in logs. Defaults to ''
+    """
+    try:
+        # Handle shell scripts on Windows by explicitly using bash
+        if isinstance(params, str) and params.endswith('.sh') and os.name == 'nt':
+            params = ['bash', params]
+        elif isinstance(params, list) and len(params) > 0 and params[0].endswith('.sh') and os.name == 'nt':
+            params = ['bash'] + params
+
+        process = subprocess.Popen(  # noqa: F821
+            params, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        )
+
+        with process.stdout:
+            for line in iter(process.stdout.readline, ''):
+                logger.trace(f'[process: {name}] {line.strip()}')
+
+    except Exception as e:
+        raise Exception(f'Failed to run {name} process', e)
+
+    exit_code = process.wait()
+    if exit_code != 0:
+        raise Exception(f'Process {name} exited with code {exit_code}')
