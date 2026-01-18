492e14d5396d0ba848e4937d3b1574371c1f1342
Ethan <119145352+Surxe@users.noreply.github.com>
2026-01-18 12:53:32 -0600

    Validate constants (#79)
    * Add functionality to util.clear_dir() such that it ignores `.git`.
    This is so that the output dir can be set as an external repository for
    diffviewing locally to ensure code changes yield the expected result
    * Removed some excess code needed for the constants & vars data
    structure
    * Validated that module classes, tags, faction, and weight/energy
    capacities are never variables and are always null or constant

--- src/parse/parsers/module.py
+++ src/parse/parsers/module.py
@@ -278,17 +278,20 @@ class Module(ParseObject):
                     
             parsed_levels.append(parsed_level)
 
-        # Categorize into constants and variables
-        categorized_parsed_levels = {}
         constants_and_variables = self._separate_constants_and_variables(parsed_levels)
-        constants = constants_and_variables.get("constants")
-        variables = constants_and_variables.get("variables")
-        if constants:
-            categorized_parsed_levels["constants"] = constants
-        if variables:
-            categorized_parsed_levels["variables"] = variables
 
-        return categorized_parsed_levels
+        # Validate that particular keys are always constants (for frontend purposes)
+        expected_constants = [
+            'module_class_id_1', 'module_class_id_2',
+            'module_tag_id_1', 'module_tag_id_2',
+            'module_faction_id',
+            'LoadCapacity', 'EnergyCapacity'
+        ]
+        for key in expected_constants:
+            if 'variables' in constants_and_variables and key in constants_and_variables["variables"]:
+                logger.error(f"Expected key {key} is a variable when it was expected to be a constant or null. For frontend purposes.")
+
+        return constants_and_variables
     
     def _separate_constants_and_variables(self, levels_data):
         """
@@ -319,10 +322,10 @@ class Module(ParseObject):
                 parsed_levels_variable_stats.append(parsed_level_variable_stats)
 
         ret = {}
-        if parsed_levels_variable_stats:
-            ret["variables"] = parsed_levels_variable_stats
         if parsed_constant_stats:
             ret["constants"] = parsed_constant_stats
+        if parsed_levels_variable_stats:
+            ret["variables"] = parsed_levels_variable_stats
         
         return ret
 
